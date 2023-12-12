e72a741c9ee904ea398c08849561c02cc018b17a
Frederic Willem <frederic.willem@gmail.com>
2023-12-12 07:37:33 +0100

    better snippet's

---
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+		     '(("elisp" "#+begin_src emacs-lisp\n$0\n#+end_src" "elisp" nil nil nil "/home/frederic/.emacs.d/snippets/org-mode/elisp" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Dec 12 07:33:49 2023

10746877fe3fdeccf1a16ffa2a8a46c8f59aa429
Frédéric Willem <frederic.willem@gmail.com>
2024-01-14 07:40:27 +0100

    update snippets

---
+++ snippets/message-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `message-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'message-mode
+		     '(("grfr" "Bonjour $1,\n\n$0\n\nBien à vous,\n\nFrédéric\n\n" "French greetings" nil nil nil "/home/willefi/.emacs.d/snippets/message-mode/grfr" nil nil)
+		       ("gren" "Hello $1,\n\n$0\n\nRegards,\n\nFrédéric\n" "English greetings" nil nil nil "/home/willefi/.emacs.d/snippets/message-mode/gren" nil nil)))
+
+
+;;; Do not edit! File generated at Sun Jan 14 07:36:03 2024
--- snippets/org-mode/.yas-compiled-snippets.el
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -2,7 +2,7 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'org-mode
-		     '(("elisp" "#+begin_src emacs-lisp\n$0\n#+end_src" "elisp" nil nil nil "/home/frederic/.emacs.d/snippets/org-mode/elisp" nil nil)))
+		     '(("eli" "#+begin_src emacs-lisp\n$0\n#+end_src" "eli" nil nil nil "/home/willefi/.emacs.d/snippets/org-mode/eli" nil nil)))
 
 
-;;; Do not edit! File generated at Tue Dec 12 07:33:49 2023
+;;; Do not edit! File generated at Sun Jan 14 07:36:03 2024
